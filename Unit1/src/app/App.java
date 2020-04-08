eff229952c9f559e71f0afcf2c79213abf02c508
johnmarquette <829641@mystma.org>
2020-04-08 18:04:28 +0000

    creating java project

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("1. I still believe);
+    }   System.out.println("2. I can only Imagine")
+}       System.out.println("3. I'm not ashamed")
+        System.out.println("4. Miracles from Heaven")
+        System.out.println("5. A Dog's Jounrey")
+        
