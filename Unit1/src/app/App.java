9653f1aa0e97613e2d8aa4160a5c4a245344cad5
johnmarquette <829641@mystma.org>
2020-04-09 19:30:20 +0000

    done

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,9 +2,33 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. I still believe);
-    }   System.out.println("2. I can only Imagine")
-}       System.out.println("3. I'm not ashamed")
-        System.out.println("4. Miracles from Heaven")
-        System.out.println("5. A Dog's Jounrey")
+      boolean livesinElkRviver = false; 
+      char firstletterofName = "J";
+      byte numberoftoes = "5";
+      short numberofmilkshakes = "10";
+      int numberofgokarts = "2,000,000,000";
+      long numberofcoolteachers = "1,000,000,000,000,000";
+      float numberofballs = "7.5";
+      double numberofpencils = "10.3333333333333";
+
+
+      System.out.println("The number of pencils is:");
+      System.out.println("numberofpencils");
+      System.out.println("The number of balls is:");
+      System.out.println("numberofballs");
+      System.out.println("The number of cool teachers is:");
+      System.out.println("numberofcoolteachers");
+      System.out.println("The number of go karts is:");
+      System.out.println("numberofgokarts");
+      System.out.println("The number of milkshakes is:");
+      System.out.println("numberofmilkshakes");
+      System.out.println("The number of toes is:");
+      System.out.println("numberoftoes");
+      System.out.println("The first letter of Name is:");
+      System.out.println("firstletterofName");
+      System.out.println("Do you live in Elk River?");
+      System.out.println("livesinElkRiver");
+      
+
+
         
