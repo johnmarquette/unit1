4c32ae0be17f87ff970b0e75bb21328903b7beb8
johnmarquette <829641@mystma.org>
2020-04-14 19:02:25 +0000

    finish

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,24 +2,35 @@ package app;
 
 public class App {
     public static void main(final String[] args) throws Exception {
-    String person1 = "john";
-    String person2 = "kaz";
-    String verb1 = "surf";
-    String verb2 = "train";
-    String verb3 = "surf";
-    String food = "pizza";
-    String dayOfWeek = "saturday";
-    String place1 = "beach";
-    String place2 = "coca Beach";
-    String adjective1 = "huge";
-    String adjective2 = "awesome";
+    int count = 0;
+    String color = "red";
+    boolean Iliveinotsego = true;
 
 
+    while (count <= 10) {
+        System.out.println("The count is" = count);
+        count = count + 1;
+    }
 
-System.out.println(person1 + "is a programmer who likes to" + verb1);
-System.out.println("Every" + dayOfWeek + "they make a" + adjective1 + food + "for breakfast and" + verb2 + "at" + place1 + "with" person2);
-System.out.println("Sometimes, they go to the" + adjective2 + place2 + "with" + person2 + "so that they can" + verb3 + "together");
+    System.out.println("count finished");
 
+    while (color == "red"){
+    System.out.println("The color is" + color);
+    color = "green"; 
+    }
+
+    System.out.println("Color finished");
+
+    while (I live in otsego = true){
+System.out.println("The city is" + city);
+city = "otsego";
+    }
+
+    System.out.println("City finished");
+    
+
+
+    
 
 
         
