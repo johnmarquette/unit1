aa8baf65bf89b6950160462f2a7a7080c0a3e8a1
johnmarquette <829641@mystma.org>
2020-04-20 16:26:19 +0000

    if loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,30 +3,17 @@ package app;
 public class App {
     public static void main(final String[] args) throws Exception {
 
-    for (int i = 22; i <= 28; i = i + 3){
-        System.out.println(i);
+    for (int i = 1; i <= 20; i++){
+        if (i % 4 == 0){
+            System.out.println(i + "is divisble by 4");
+        } else {
+            System.out.println(i + "is not divisble by 4");
+        }
     }
-
-    for (int i = 2; i <= 32; i = i * 2){
-        System.out.println(i);
-    }
-
-    for (int i = 3; i <= -81; i = i * -i){
-        System.out.println(i);
-    }
-
-    for (int i = 50; i <= 5; i = i - 5){
-        System.out.println(i);
-    }
-
-    for (int i = 1; i <= 9; i = i + 1){
-        System.out.println(i * i);
-        System.out.println(i + 3);
-        System.out.println(i);
         
     }
 } 
-}
+
 
 
 
