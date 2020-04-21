607606a30f18ee8b8ad6fe4d0aa14153b462fa78
johnmarquette <829641@mystma.org>
2020-04-21 18:04:49 +0000

    for loop challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,17 +2,22 @@ package app;
 
 public class App {
     public static void main(final String[] args) throws Exception {
-
-    for (int i = 1; i <= 20; i++){
-        if (i % 4 == 0){
-            System.out.println(i + "is divisble by 4");
-        } else {
-            System.out.println(i + "is not divisble by 4");
+    System.out.println("start first loop");
+    for (int i = 0; i <= 9; i ++){
+        if (i == 10){
+            System.out.println("repeat #10, 6 times");
+        } for (int i = 11; i <= 19; i ++){
+            System.out.println("end first loop");   
         }
+        }
+    System.out.println("start first loop");
+    for (i = -1; i <= 1; i * -1){
+        System.out.println()
+    }
     }
         
     }
-} 
+ 
 
 
 
