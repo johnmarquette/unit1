a9ba6cae453edaf563b630d3cdc76bd781aa3b87
johnmarquette <829641@mystma.org>
2020-04-23 17:55:12 +0000

    for loop challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,8 +2,10 @@ package app;
 
 public class App { 
     public static void main(final String[] args) throws Exception {
-System.out.println("start first loop");
-    for (int i = 20; i < 0; i ++){
+    
+        System.out.println("start first loop");
+    for (int i = 20; i > 0; i ++){
+        System.out.println(i);
         if (i == 10){
             for (int j = 0; j < 5; j++){
                  System.out.println(i);
@@ -13,19 +15,16 @@ System.out.println("start first loop");
     }
     System.out.println("end first loop");   
     System.out.println("start second loop");
-    System.out.println("Hey!");
     for (int i = 0; i <= 36; i = i + 4){
-        if (i %6 == 0);
-    
-
-    
-        
         System.out.println(i);
-        
-
-
+        if (i % 12 == 0){
+            System.out.println("Hey!");
+        }
+    
     }
-       System.out.println("end second loop"); 
+    System.out.println("End second loop");
+
+        
     
     }
 }
