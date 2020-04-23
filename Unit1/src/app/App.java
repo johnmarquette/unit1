7fc3072257b8556142bab58474a1bd56ecf4a100
johnmarquette <829641@mystma.org>
2020-04-23 19:51:00 +0000

    second for loop challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,26 +3,28 @@ package app;
 public class App { 
     public static void main(final String[] args) throws Exception {
     
-        System.out.println("start first loop");
-    for (int i = 20; i > 0; i ++){
-        System.out.println(i);
-        if (i == 10){
-            for (int j = 0; j < 5; j++){
-                 System.out.println(i);
-                 
+        System.out.println("Double or single digit");
+        for (int i =20; i > 1; i = i - 1){
+            System.out.println(i + " is a double digit");
+            if (i < 10){
+                System.out.println(i + "is a single digit");
+            }
         }
-      }  
-    }
-    System.out.println("end first loop");   
-    System.out.println("start second loop");
-    for (int i = 0; i <= 36; i = i + 4){
-        System.out.println(i);
-        if (i % 12 == 0){
-            System.out.println("Hey!");
+        System.out.println("Nice");
+        System.out.println("Fast Food");
+        
+        for (int i = 0; i < 5;){
+            System.out.println(i);
+            if (i % 5 == 0){
+            System.out.println("Burgers");
+            System.out.println("Fries");
         }
-    
-    }
-    System.out.println("End second loop");
+         
+        }
+        System.out.println("Nice");     
+        
+         
+
 
         
     
