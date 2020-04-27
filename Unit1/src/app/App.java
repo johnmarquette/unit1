a1b92da5d881163880ba83abae5b924e1bfc54d4
johnmarquette <829641@mystma.org>
2020-04-27 16:17:41 +0000

    practice from test

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -8,20 +8,21 @@ public class App {
             System.out.println(i);
             if (i == 5){ 
                 System.out.println("five");
-            }
+            }if (i == 150){
+                }
+            else { }
         }
             System.out.println("End first loop");
             System.out.println("Start second loop");
-            for (int i = 100; i <= 150; i = i + 10){
+            for (int i = 100; i < 201; i = i + 10){
                 System.out.println(i);
-            }
-            for (int i = 150; i <= 200; i = i + 1){
-                System.out.println(i);
-                if (i == 160){
-                }else {
-                    System.out.println(i = i + 10);
+                if (i == 150){
+                    for (j = 150; j < 160; j = j + 1){
+                        System.out.println(j);
+                    }
                 }
             }
+            
             
             System.out.println("End second loop");
             
