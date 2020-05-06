a690245883122f8d45345e871a010b30061a75eb
johnmarquette <829641@mystma.org>
2020-05-06 19:05:30 +0000

    mad lib input

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,27 +6,27 @@ public class App {
     
         Scanner input = new Scanner(System.in);
 
-        System.out.println("What is the color of the sky?");
-        String color = input.nextLine();
-        if (color.equals("blue")){
-            System.out.println("Correct");
-        }else {
-            System.out.println("incorrect");
-        }
-        System.out.println("What is the capitol of Calfornia?");
-        String capitol = input.nextLine();
-        if (capitol.equals("Sacramento")){
-            System.out.println("Correct");
-        }else {
-            System.out.println("incorrect");
-        }
-        System.out.println("Goldfish have a memory of?");
-        String memory = input.nextLine();
-        if (memory.equals("3 seconds")){
-            System.out.println("Correct");
-        }else {
-            System.out.println("incorrect");
-        }
+        System.out.println("Please enter a noun.");
+        String noun = input.nextLine();
+        System.out.println("Please enter a name.");
+        String name = input.nextLine();
+        System.out.println("Please enter a verb.");
+        String verb = input.nextLine();
+        System.out.println("Please enter an adjective.");
+        String adjective = input.nextLine();
+        System.out.println("Please enter a place.");
+        String place = input.nextLine();
+        System.out.println("Please enter a food.");
+        String food = input.nextLine();
+        System.out.println("Please enter an animal.");
+        String animal = input.nextLine();
+        System.out.println("Please enter a number.");
+        String number = input.nextLine();
+        System.out.println("Please enter a candy.");
+        String candy = input.nextLine();
+        System.out.println("Please enter a movie.");
+        String movie = input.nextLine();
+        System.out.println("Make a sentence.");
         
         
         
