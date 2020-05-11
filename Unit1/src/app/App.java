aea4561e053617e213319a7b783149feb81e9e58
johnmarquette <829641@mystma.org>
2020-05-11 17:10:54 +0000

    simple calculator

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,31 +6,43 @@ public class App {
     
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter a noun.");
-        String noun = input.nextLine();
-        System.out.println("Please enter a name.");
-        String name = input.nextLine();
-        System.out.println("Please enter a verb.");
-        String verb = input.nextLine();
-        System.out.println("Please enter an adjective.");
-        String adjective = input.nextLine();
-        System.out.println("Please enter a place.");
-        String place = input.nextLine();
-        System.out.println("Please enter a food.");
-        String food = input.nextLine();
-        System.out.println("Please enter an animal.");
-        String animal = input.nextLine();
-        System.out.println("Please enter a number.");
-        String number = input.nextLine();
-        System.out.println("Please enter a candy.");
-        String candy = input.nextLine();
-        System.out.println("Please enter a movie.");
-        String movie = input.nextLine();
-        System.out.println("A" + noun + "named" + name + verb + adjective + "at the" + place);
-        System.out.println("And his favorite food is" + food);
-        System.out.println("Also he goes to the zoo to see an" + animal);
-        System.out.println(name + "is" + number + "years old");
-        System.out.println("His favorite candy is" + candy + "when watching his favorite movie which is" + movie);
+        System.out.println("Please enter number for a.");
+         String numba = input.nextLine();
+        System.out.println("Please enter number for b.");
+        String numbb = input.nextLine();
+        System.out.println("Please enter function");
+        String func = input.nextLine();
+        System.out.println(numba + func + numbb);
+        System.out.println(5 + 5);
+        System.out.println("Please enter function");
+        String funct = input.nextLine();
+        System.out.println(numba + funct + numbb);
+        System.out.println(5 - 5);
+        System.out.println("Please enter function");
+        String functi = input.nextLine();
+        System.out.println(numba + functi + numbb);
+        System.out.println(5 * 5);
+        System.out.println("Please enter function");
+        String function = input.nextLine();
+        System.out.println(numba + function + numbb);
+        System.out.println(5 / 5);
+
+        
+
+        
+        
+        
+        
+       
+        
+        
+
+
+
+
+
+
+       
         
         
         
