88e2617f4ec896ad1f1be5912b5569cb6563452d
johnmarquette <829641@mystma.org>
2020-05-18 18:11:40 +0000

    Dogs

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,55 +3,28 @@ import java.util.Scanner;
 
 public class App { 
     public static void main(final String[] args) throws Exception {
-    
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter a number.");
-        int num1 = input.nextInt();
-        System.out.println("Enter another number.");
-        int num2 = input.nextInt();
-
-        System.out.println("Press 1 for addition, 2 for subtraction, 3 for mutiplication, or 4 for division.");
-        int choice = input.nextInt();
-
-        if (choice == 1){
-            System.out.println(num1 + num2);
-        }
-        if (choice == 2){
-            System.out.println(num1 - num2);
-        }
-        if (choice == 3){
-            System.out.println(num1 * num2);
-        }
-        if (choice == 4){
-            System.out.println(num1 / num2);
-        }
-
-        
-
-        
-        
-        
-        
-       
-        
-        
-
-
-
-
-
-
-       
-        
-        
-        
-        
-        
-        
-        
-            
-            
+    Dog dog1 = new Dog();
+    Dog dog2 = new Dog();  
+    Dog dog3 = new Dog();
+    Scanner input = new Scanner(System.in);
+    dog1.name = "Kevin";
+    System.out.println(dog1.name);
+    dog1.breed = "Golden Retriever";
+    System.out.println(dog1.breed);
+    dog1.age = 8;
+    System.out.println(dog1.age);
+    dog2.name = "Max";
+    System.out.println(dog2.name);
+    dog2.breed = "German Shepherd";
+    System.out.println(dog2.breed);
+    dog2.age = 6;
+    System.out.println(dog2.age);
+    dog3.name = "Barry";
+    System.out.println(dog3.name);
+    dog3.breed = "Siberian Husky";
+    System.out.println(dog3.breed);
+    dog3.age = 9;
+    System.out.println(dog3.age);
     }
 }
 
---
+++ Unit1/src/app/Dog.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Person{
+
+    String name;
+    String breed;
+    int age;
+}
