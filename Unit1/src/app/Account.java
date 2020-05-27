ef0fe70270246fb24961dc1b0bca5b9e69288952
johnmarquette <829641@mystma.org>
2020-05-27 16:17:21 +0000

    Bank Account

---
+++ Unit1/src/app/Account.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Account{
+
+    String name;
+    int startingamount;
+    String password;
+
+    
+
--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,46 +3,31 @@ import java.util.Scanner;
 
 public class App { 
     public static void main(final String[] args) throws Exception {
-    Dog dog1 = new Dog();
-    Dog dog2 = new Dog();  
-    Dog dog3 = new Dog();
+    Account account1 = new Account();
     Scanner input = new Scanner(System.in);
-    dog1.name = "Kevin";
-    System.out.println(dog1.name);
-    dog1.breed = "Golden Retriever";
-    System.out.println(dog1.breed);
-    dog1.age = 8;
-    System.out.println(dog1.age);
-    dog1.gender = "Male";
-    System.out.println(dog1.gender);
-    dog1.haircolor = "Black";
-    System.out.println(dog1.haircolor);
-    dog1.eyecolor = "Blue";
-    System.out.println(dog1.eyecolor);
-    dog2.name = "Max";
-    System.out.println(dog2.name);
-    dog2.breed = "German Shepherd";
-    System.out.println(dog2.breed);
-    dog2.age = 6;
-    System.out.println(dog2.age);
-    dog2.gender = "Female";
-    System.out.println(dog2.gender);
-    dog2.haircolor = "Blonde and Black";
-    System.out.println(dog2.haircolor);
-    dog2.eyecolor = "Hazelnut";
-    System.out.println(dog2.eyecolor);
-    dog3.name = "Barry";
-    System.out.println(dog3.name);
-    dog3.breed = "Siberian Husky";
-    System.out.println(dog3.breed);
-    dog3.age = 9;
-    System.out.println(dog3.age);
-    dog3.gender = "Male";
-    System.out.println(dog3.gender);
-    dog3.haircolor = "Gray and White";
-    System.out.println(dog3.haircolor);
-    dog3.eyecolor = "Cyran";
-    System.out.println(dog3.eyecolor);
+    System.out.println("Create Account");
+    account1.name = "John Marquette";
+    System.out.println(account1.name);
+    account1.startingamount = 500;
+    System.out.println(account1.startingamount);
+    account1.password = "Ollie";
+    System.out.println(account1.password);
+    System.out.println("Account created, please log in");
+    System.out.println("Enter your name");
+    String name = input.nextLine();
+    System.out.println("Enter your password");
+    if (name == "John Marquette"){
+    }
+    if (password == "Ollie"){
+        System.out.println("Access Granted, welcome John Marquette");
+    }
+    System.out.println("What would you like to do?");
+    System.out.println("Press 1 to check balance, 2 to withdraw money, 3 to change password.");
+    int choice = input.nextInt();
+    System.out.println("How much money would you like to withdraw?");
+    int money = input.nextInt();
+    System.out.println("You withdrew 200 dollars.");
+    System.out.println("Your current balance is now 300.");
     }
 }
 
